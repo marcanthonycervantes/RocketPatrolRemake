02fd92df429ae1d615f8846a8ddbe1fb96ce0543
Marc Anthony Cervantes <mcerva18@ucsc.edu>
2021-04-08 22:57:05 -0700

    Static starfield background addition

--- rocket_remake/src/scenes/Play.js
+++ rocket_remake/src/scenes/Play.js
@@ -2,7 +2,15 @@ class Play extends Phaser.Scene {
     constructor() {
         super("playScene");
     }
+
+    preload() {
+        this.load.image('starfield', 'assets/starfield.png');
+    }
+
     create() {
+
+        this.starfield = this.add.tileSprite(0,0,640,480, 'starfield').setOrigin(0,0);
+
         //green UI background
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00, ).setOrigin(0,0);
 
@@ -11,6 +19,7 @@ class Play extends Phaser.Scene {
 	    this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
 	    this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
 	    this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
+
 
     }
 }
